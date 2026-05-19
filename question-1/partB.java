fba3643ea0635297361348ee7ed66b2fdd04cbdb
shak <shakhrambekg2789@nycstudents.net>
2026-05-19 10:15:36 -0400

    Implement getShortenedName method
    Added method to return a shortened version of username.

--- question-1/partB.java
+++ question-1/partB.java
@@ -1,1 +1,15 @@
+/**
+* Returns a shortened version of username with each hyphen ("-")
+* and the character before it removed, as described in part (b)
+* Preconditions: username does not start or end with a hyphen.
+* username does not contain consecutive hyphens.
+* username.length() >= 2
+* Postcondition: username is unchanged.
+*/
+public String getShortenedName() {
+  String newName = "";
 
+  
+}
+/* There may be instance variables, constructors, and methods
+that are not shown. */
