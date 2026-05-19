da2a62bcfc0442231b87e2582d28c50d3371a34b
shak <shakhrambekg2789@nycstudents.net>
2026-05-19 10:11:32 -0400

    Add username availability check in Account constructor
    Implement username creation logic to ensure availability.

--- question-1/partA.java
+++ question-1/partA.java
@@ -1,1 +1,24 @@
+private String username; // To be initialized in part (a)
+/**
+* Creates a username based on the parameter requestedName. If the
+* username is unavailable, appends successive integers, beginning
+* with 1, to requestedName until an available username is found,
+* as described in part (a).
+*/
+public Account(String requestedName) { 
+  
+  username = requestedName;
+  int version = 1;
 
+  while (!isAvailable(username)) { // while username is not available, add 1 to the end of the name until its available
+    username = requestedName + version; //Ex: Unavailable = Shak, Available = Shak2872
+    i++;
+  }
+
+}
+/**
+* Returns true if the parameter str is an available username;
+* returns false otherwise.
+*/
+public static boolean isAvailable(String str)
+{ /* implementation not shown */ }
