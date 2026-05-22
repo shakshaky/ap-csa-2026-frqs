9c211f085a73b5c41926571ac5141f0c73e059de
shak <shakhrambekg2789@nycstudents.net>
2026-05-22 10:01:29 -0400

    Implement Bottle class with capacity and update method
    Added a Bottle class with capacity and amount attributes. Implemented a method to update the amount and refill if below a threshold.

--- question-2/Bottle.java
+++ question-2/Bottle.java
@@ -1,1 +1,21 @@
+public class Bottle {
 
+  private double capacity;
+  private double amount;
+  
+  public Bottle(double capacity) {
+    this.capacity = capacity;
+    amount = capacity;
+  }
+
+  public double updateAmount(double amt) {
+    amount -= amt; // newAmount - 750.0 = 250.0
+
+    if (amount < 0.25 * capacity) { // if the new amount is less than 250.0, then refill the bottle
+      amount = capacity; // 250.0 is set to 1000.0 all over again
+    }
+
+    return amount;
+  }
+  
+}
