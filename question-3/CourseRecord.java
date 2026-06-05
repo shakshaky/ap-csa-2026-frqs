9dcff816d96752858f079dc88af872f8b84a05ab
shak <shakhrambekg2789@nycstudents.net>
2026-06-05 09:42:07 -0400

    Implement moreHistoryThanMathAbsences method

--- question-3/CourseRecord.java
+++ question-3/CourseRecord.java
@@ -1,2 +1,19 @@
-public int moreHistoryThanMathAbsences()
-{ /* to be implemented */ }
+public int moreHistoryThanMathAbsences() {
+  int count = 0;
+  
+  for (CourseRecord history : historyList) {
+    
+    for (CourseRecord math : mathList) {
+
+      if math.getStudentID().equals(history.getStudentID()) {
+        if (history.getAbsences() > math.getAbsences()) {
+          count++
+        }
+      }
+      
+    }
+    
+  }
+  
+  return count;
+}
